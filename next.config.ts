1c7566fce50b977447e87c90e6066715266d3dfc
tkhashi <pupodenews@gmail.com>
2026-05-10 23:27:26 +0900

    描画バグ修正

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@mastra/core", "@mastra/ai-sdk"],
 };
 
 export default nextConfig;
